7f26c8bd02e6e962e60237a184d9fd70d6c2c86a
Moacir Guedes <guedesmoacir12@gmail.com>
2023-09-01 11:01:05 -0300

    feat: initial commit

---
+++ frontend/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home', //Nome da aba
+    component: () => import('@/views/HomeView.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.name;
+  next();
+});
+
+export default router
