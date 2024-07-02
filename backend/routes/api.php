95e1f6fa5341a18c0b97796ab35ed0fe271a08a5
matheusk093 <kocotemmatheus@gmail.com>
2024-07-02 17:43:45 -0300

    precisa de models

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,29 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-
-Route::post('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/login', [AuthController::class, 'login'] );
-
-
-
-
+Route::get('/login', [AuthController::class,'login']);
